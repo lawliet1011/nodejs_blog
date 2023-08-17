593fb1d832afe384aa3fe345576eadd5e350e709
lawliet1011 <ezreflex12@gmail.com>
2023-08-18 00:05:20 +0700

    update 18/8

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const handlebars = require("express-handlebars");
+const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
@@ -10,26 +10,29 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //Routes init
 route(app);
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
